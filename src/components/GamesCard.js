d2c6342ff92b90ce783911456c80c2840ae6d5c3
Ashlee Crusco <ashleecrusco@gmail.com>
2017-12-03 15:27:37 -0500

    card layout complete

--- src/components/GamesCard.js
+++ src/components/GamesCard.js
@@ -23,42 +23,48 @@ class GamesCard extends Component {
 
   render() {
     return (
-      <div className="card">
-        <div className="content">
-          <div className="right floated meta">Own</div>
-          {this.props.game.game.name}
-        </div>
-        <div className="blurring dimmable image">
-          <div className="ui dimmer">
-            <div className="content">
-              <div className="center">
-                <div className="ui inverted button">Add friend</div>
+      <fragment>
+        <div className="ui card">
+          {this.state.clicked ? (
+            <div className="content fluid">
+              <div className="header">{this.props.game.game.name}</div>
+              <i className="right floated like icon" />
+              <i className="right floated star icon" />
+              <div className="left floated meta">
+                <span onClick={this.redirectToShow} className="meta">
+                  Click for more info...
+                </span>
+              </div>
+
+              <div
+                className="extra content"
+                onClick={this.handleClick.bind(this)}
+              >
+                <div className=" centered description">
+                  {" "}
+                  <br />
+                  <br />
+                  <p>{this.props.game.game.description.slice(0, 400)}...</p>
+                  <p />
+                </div>
               </div>
             </div>
-          </div>
-          <img
-            src={this.props.game.game.image_url}
-            onClick={this.redirectToShow}
-          />
-        </div>
-        <div className="content">
-          <span className="right floated">
-            <i className="heart outline like icon" />
-            {this.props.game.likes}
-          </span>
-        </div>
-        <div className="extra content">
-          {this.state.clicked ? (
-            <div onClick={this.handleClick.bind(this)} className="description">
-              {this.props.game.game.description}
-            </div>
           ) : (
-            <div onClick={this.handleClick.bind(this)} className="description">
-              Click for more info...
+            <div
+              className="centered content fluid image"
+              onClick={this.handleClick.bind(this)}
+            >
+              <img src={this.props.game.game.image_url} />
             </div>
           )}
+          <div className="extra content">
+            <div className="ui bottom attached button">
+              <i className="add icon" />
+              Add to collection
+            </div>
+          </div>
         </div>
-      </div>
+      </fragment>
     );
   }
 }
--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -5,7 +5,7 @@ const GamesList = props => {
   const gamesListCards = props.games.map(game => {
     return <GamesCard game={game} key={game.game.id} />;
   });
-  return <div className="ui special cards">{gamesListCards}</div>;
+  return <div className="ui cards">{gamesListCards}</div>;
 };
 
 export default GamesList;
